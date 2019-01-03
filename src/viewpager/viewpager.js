660d07dffbc431b6ff7d74c07fca44841dffbaff
jiao <chnhongjj@163.com>
2019-01-03 09:25:47 +0800

    feat: load image dynamical

--- src/viewpager/viewpager.js
+++ src/viewpager/viewpager.js
@@ -10,6 +10,8 @@ window.onload = function () {
 
   var index = 0;   //保存当前亮显的span标签索引值
 
+  var onShow = false; // 用来标志是否在动画中
+
   //第一种实现方式
   // next.onclick = function () {
   //   imgList.style.left = parseInt(imgList.style.left) + 800 + 'px';
@@ -51,19 +53,36 @@ window.onload = function () {
 
   //第三种实现方式
   function clickEvent(offset) {
+
     var newLeft = parseInt(imgList.style.left) + offset;   //求出新的left值
 
-    //如果newLeft值为最后一张图的附图，就更新newLeft值为最后一张图
-    if (newLeft > -800) {
-      newLeft = -4800;
+    var allTime = 300; //动画总时间
+    var interval = 10; //动画间隔时间
+    var everyOffset = offset / (allTime / interval); //每次位移量
+
+
+    // 定义内部函数动态显示图片
+    function showFunction() {
+
+      // 如果满足动画条件，就动画显示图片
+      if ((everyOffset < 0 && parseInt(imgList.style.left) > newLeft) || (everyOffset > 0 && (parseInt(imgList.style.left) < newLeft))) {
+        imgList.style.left = parseInt(imgList.style.left) + everyOffset + 'px';
+        setTimeout(showFunction, interval);
+      }
+      else {
+        //如果newLeft值为最后一张图的附图，就更新newLeft值为最后一张图
+        if (newLeft > -800) {
+          newLeft = -4800;
+        }
+        //如果newLeft值为第一张图的附图，就更新newLeft值为第一张图
+        if (newLeft < -4800) {
+          newLeft = -800;
+        }
+        imgList.style.left = newLeft + 'px';
+      }
     }
 
-    //如果newLeft值为第一张图的附图，就更新newLeft值为第一张图
-    if (newLeft < -4800) {
-      newLeft = -800;
-    }
-
-    imgList.style.left = newLeft + 'px';     //更新图片的left值
+    showFunction();
   }
 
   //添加右箭头点击事件
@@ -77,7 +96,9 @@ window.onload = function () {
     }
 
     showButton();   //调用亮显事件
-    clickEvent(800);
+    if (!onShow) {   // 如果不在动画中，就调用点击事件
+      clickEvent(800);
+    }
   }
 
   //添加左箭头点击事件
@@ -89,7 +110,9 @@ window.onload = function () {
     }
 
     showButton();  //调用亮显事件
-    clickEvent(-800);
+    if (!onShow) {   // 如果不在动画中，就调用点击事件
+      clickEvent(-800);
+    }
   }
 
   //按钮点击事件
@@ -100,7 +123,10 @@ window.onload = function () {
       }
       var thisIndex = this.getAttribute('index');
       var indexOffset = parseInt(800 * (thisIndex - index - 1));
-      clickEvent(indexOffset);
+      if (!onShow) {   // 如果不在动画中，就调用点击事件
+        clickEvent(indexOffset);
+      }
+      // clickEvent(indexOffset);
       index = thisIndex - 1;
       showButton();
       // debugger;
