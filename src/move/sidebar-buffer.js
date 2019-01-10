31786e59d717596a43774037113f43701d9bc38d
liquan <chnliquan@163.com>
2019-01-10 16:58:43 +0800

    feat: buffer sidebar

---
+++ src/move/sidebar-buffer.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+  var shareSidebar = document.getElementById('sidebar');
+
+  shareSidebar.onmouseover = function () {
+    startMove(0);
+  }
+
+  shareSidebar.onmouseout = function () {
+    startMove(-150);
+  }
+}
+
+var timer = null; // 定时器
+var speed = 0; // 速度
+
+function startMove(target) {
+  clearInterval(timer);
+
+  timer = setInterval(function () {
+    var sidebar = document.getElementById('sidebar');
+    var currentOffsetLeft = sidebar.offsetLeft; // 获取当前的位置
+
+    speed = (target - currentOffsetLeft) / 4;
+    speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+    
+    if (currentOffsetLeft == target) {
+      clearInterval(timer);
+    } else {
+      sidebar.style.left = currentOffsetLeft + speed + 'px';
+    }
+  }, 30);
+
+}
