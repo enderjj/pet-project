c96098155bc8d1b513eca360bd7631ba8219a700
liquan <chnliquan@163.com>
2019-01-10 16:43:22 +0800

    fix: fix shaking around target

--- src/move/move2.js
+++ src/move/move2.js
@@ -1,14 +1,14 @@
-window.onload = function() {
+window.onload = function () {
   var moveToThree = document.getElementById('start-move1');
   var moveToSix = document.getElementById('start-move2');
   var targetThree = document.getElementById('move-target1');
   var targetSix = document.getElementById('move-target2');
-  
-  moveToThree.onclick = function() {
+
+  moveToThree.onclick = function () {
     startMove(targetThree.offsetLeft);
   }
 
-  moveToSix.onclick = function() {
+  moveToSix.onclick = function () {
     startMove(targetSix.offsetLeft);
   }
 }
@@ -21,17 +21,22 @@ function startMove(target) {
 
   var moveElement = document.getElementById('moveContainer'); // 获取要移动的元素
 
-  timer = setInterval(function() {
+  timer = setInterval(function () {
     var currentOffsetLeft = moveElement.offsetLeft;
 
-    if(currentOffsetLeft < target) {
+    if (currentOffsetLeft < target) {
       speed = 7;
-    }else {
+    } else {
       speed = -7;
     }
 
-    moveElement.style.left = currentOffsetLeft + speed + 'px';
-  }, 30);
+    if (Math.abs(target - currentOffsetLeft) < 7) { // 如果当前位置距离目标足够近（这里小于速度），就停止运动，并设置当前位置为目标位置
+      clearInterval(timer);
+      moveElement.style.left = target + 'px';
+    } else {
+      moveElement.style.left = currentOffsetLeft + speed + 'px';
+    }
 
+  }, 30);
 
 }
