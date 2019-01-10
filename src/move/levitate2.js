cf54a875a1d5b10d0fcf57764ce12eb40db694d7
enderjj <chnhongjj@163.com>
2019-01-10 15:56:10 +0800

    feat: scroll on middle

--- src/move/levitate2.js
+++ src/move/levitate2.js
@@ -1,5 +1,29 @@
 window.onscroll = function () {
   var scrollElement = document.getElementById('scroll-element');
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  scrollElement.style.top = (document.documentElement.clientHeight - scrollElement.offsetHeight) / 2;
+  // scrollElement.style.top = (document.documentElement.clientHeight - scrollElement.offsetHeight) / 2 + scrollTop + 'px';
+  startScroll(parseInt((document.documentElement.clientHeight - scrollElement.clientHeight) / 2) + scrollTop);
+}
+
+var timer = null; // 定时器
+
+function startScroll(target) {
+  clearInterval(timer);
+
+  var scrollEle = document.getElementById('scroll-element');
+  var scrollPx = document.getElementById('scroll-px');
+
+  timer = setInterval(function () {
+    var currentOffsetTop = scrollEle.offsetTop; // 把元素当前的 offsetTop 取出来
+    var speed = (target - currentOffsetTop) / 4;
+
+    speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+    if (currentOffsetTop === target) {
+      clearInterval(timer);
+    } else {
+      scrollPx.value = currentOffsetTop;
+      scrollEle.style.top = currentOffsetTop + speed + 'px';
+    }
+  }, 30);
 }
